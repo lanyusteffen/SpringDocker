19efff12a8bc9566805356623cff98a889e5527b
lanyusteffen <lanyusteffen@gmail.com>
2017-11-14 18:08:33 +0800

    Initial import for window platform running script!

--- build-docker.bat
+++ build-docker.bat
@@ -2,5 +2,7 @@
 set base_dir=%~dp0
 %base_dir:~0,2%
 pushd %base_dir%
-docker build -t stelylanlife/spring-boot-docker %~dp0/build/docker
+copy /y %~dp0src\main\docker\Dockerfile %~dp0build\libs\Dockerfile
+docker build -t stelylanlife/spring-boot-docker %~dp0build\libs
+del %~dp0build\libs\Dockerfile
 popd
