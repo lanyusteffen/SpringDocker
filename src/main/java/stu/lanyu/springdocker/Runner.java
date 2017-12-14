23500051efb6563fa26c76abff8c73c83ae541e0
lanyusteffen@gmail.com <lanyu1986_4120>
2017-12-14 23:10:38 +0800

    处理当当分片效果

--- src/main/java/stu/lanyu/springdocker/Runner.java
+++ src/main/java/stu/lanyu/springdocker/Runner.java
@@ -2,7 +2,6 @@ package stu.lanyu.springdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
