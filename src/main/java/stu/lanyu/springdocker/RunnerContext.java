91544dd4c5c68c44960b43873fb4bebdea17e20e
lanyusteffen@gmail.com <lanyu1986_4120>
2017-12-30 11:17:19 +0800

    修复RunnerContext未在Spring初始化注入

--- src/main/java/stu/lanyu/springdocker/RunnerContext.java
+++ src/main/java/stu/lanyu/springdocker/RunnerContext.java
@@ -3,7 +3,9 @@ package stu.lanyu.springdocker;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RunnerContext implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
