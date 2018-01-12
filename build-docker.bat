27b803b254187fcb749ee422a0efa6569c53d047
lanyusteffen <lanyusteffen@gmail.com>
2018-01-12 18:02:49 +0800

    调整Postgres

--- build-docker.bat
+++ build-docker.bat
@@ -3,6 +3,6 @@ set base_dir=%~dp0
 %base_dir:~0,2%
 pushd %base_dir%
 copy /y %~dp0src\main\docker\Dockerfile %~dp0build\libs\Dockerfile
-docker build -t stelylanlife/spring-boot-docker %~dp0build\libs
+docker build -t stelylan/spring-boot-docker %~dp0build\libs
 del %~dp0build\libs\Dockerfile
 popd
--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 set base_dir=%~dp0
 %base_dir:~0,2%
 pushd %base_dir%
-docker run -d -p 8080:8080 --name spring_docker_api --link postgres_postgres-master_1:postgres_postgres-master_1 --link postgres_postgres-slave_1:postgres_postgres-slave_1 --link redis_docker-redis-sentinel1_1:redis_docker-redis-sentinel1_1 --link redis_docker-redis-sentinel2_1:redis_docker-redis-sentinel2_1 --link redis_docker-redis-sentinel3_1:redis_docker-redis-sentinel3_1 -lt stelylanlife/spring-boot-docker
+docker run -d -p 8080:8080 --name spring_docker_api --link postgres_pg-master_1:postgres_pg-master_1 --link postgres_pg-slave_1:postgres_pg-slave_1 --link redis_docker-redis-sentinel1_1:redis_docker-redis-sentinel1_1 --link redis_docker-redis-sentinel2_1:redis_docker-redis-sentinel2_1 --link redis_docker-redis-sentinel3_1:redis_docker-redis-sentinel3_1 -lt stelylan/spring-boot-docker
 popd
 
 
