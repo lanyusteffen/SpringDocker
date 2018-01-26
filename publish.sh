5db69ffc801668185dd99a5f5ed295691642a41f
lanyusteffen@gmail.com <lanyu1986_4120>
2018-01-26 17:21:32 +0800

    发表docker仓储脚本

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ cd $SPRING_PROJECT_PATH/SpringDocker
 echo 开始编译Spring项目并创建Docker镜像!
 ./gradlew buildDocker
 echo 设置上传仓储镜像Tag
-docker tag stelylan/task-manager $REPOSITORY_URL/stelylan/spring-boot-docker
+docker tag stelylan/spring-boot-docker $REPOSITORY_URL/stelylan/spring-boot-docker
 echo 上传镜像到仓储
 docker push $REPOSITORY_URL/stelylan/spring-boot-docker
 echo 清除临时镜像
