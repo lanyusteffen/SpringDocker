5e1e777c545d295f2c07df6019ee4f8767f7243c
lanyusteffen <lanyusteffen@gmail.com>
2018-03-26 23:38:31 +0800

    1. 修复升级Spring boot 2.0后JPA异常: createClob 2. Redis Message部分从Jedis更新为Letucce

--- src/main/java/stu/lanyu/springdocker/config/RedisTemplateConfig.java
+++ src/main/java/stu/lanyu/springdocker/config/RedisTemplateConfig.java
@@ -1,21 +1,36 @@
 package stu.lanyu.springdocker.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.resource.ClientResources;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import stu.lanyu.springdocker.domain.User;
 import stu.lanyu.springdocker.redis.RedisObjectSerializer;
 
+import java.time.Duration;
+import java.util.Optional;
+
 @Configuration
 public class RedisTemplateConfig {
 
+    @Autowired(required = true)
+    private RedisMessageProperties redisMessageProperties;
+
     private LettuceConnectionFactory getLettuceConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory =
-            new LettuceConnectionFactory();
-        lettuceConnectionFactory.afterPropertiesSet();
-        return lettuceConnectionFactory;
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisMessageProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisMessageProperties.getPort());
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisMessageProperties.getPassword()));
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
